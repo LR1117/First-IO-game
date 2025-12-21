2f753b070ef123e0b6bd6301db0749c57ffd199e
LR1117 <liamri28@sssas.org>
2025-12-21 15:22:32 -0500

    made player fallow mouse

--- Client/main.js
+++ Client/main.js
@@ -1,50 +1,59 @@
 var c = document.getElementById('gameCanvas');
 var ctx = c.getContext('2d');
 
-var playerstartx=10;
-var playerstarty=100;
+var playerspeed=1;
+var playerStartSpeed =1;
 
-var playerspeed=5;
-var playerStartSpeed =5;
-
-var playerx=playerstartx;
-var playery=playerstarty;
+var playerx=playerstartx =0;
+var playery=playerstarty =0;
 
 var cc = document.getElementById("canvasContatiner");
+var posElement = document.getElementById('Position');
 
-function drawPlayer(x,y) {
+document.addEventListener('mousemove', (event) => {
+    playerx = event.clientX;
+    playery = event.clientY;
+    posElement.textContent = `Your mouse position is X: ${x}, Y: ${y}`;
+});
+
+
+function drawPlayer() {
+
 ctx.beginPath();
 ctx.fillStyle = 'green';
-ctx.rect(x, y, 5, 5);
+ctx.rect(playerx, playery, 50, 50);
 ctx.fill();
 
 }
+
 function animatePlayer(){
     ctx.clearRect(0,0,c.width,c.height);
+    
+    
 
-    playerx += playerspeed;
+    drawPlayer();
 
-    drawPlayer(playerx,playery);
+    console.log(playerx);
 
     requestAnimationFrame(animatePlayer);
 }
+
 function toggleCanvas(){
-   
+    
     if (cc.style.display == "none"){
-        playerspeed=playerStartSpeed;
-        playerx = playerstartx;
 
         cc.style.display = "block";
-        drawPlayer();
+       
+   
         animatePlayer();
+        
+
     }else{
         
         cc.style.display = "none";
         
     }
     //FIXME: Problem with the animation, speeds up each time the thing is toggled
-
-
 }
 
 
